45d66c6414cf7849216e674fd7333f123408036b
Zachary Uzupis <zuzupis@gmail.com>
2016-02-22 10:11:35 -0500

    fixed nav bar float issues

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   var index = 0;
-  var $review = $(".review")[index];
+  var review = $(".review")[index];
 
   function displayReviews(){
     // the root of why my reviews won't start. I keep getting the error $review.is is not a function. why!?
     // I beleive my logic is right, there's gotta some jquery rule that i'm not getting around.
-    if ($review.is(':hidden')){
-      $review.fadeIn('slow', function(){
+    if ($(".review").is(':hidden')){
+      $(".review").fadeIn('slow', function(){
 
       });
       index ++
